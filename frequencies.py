75ee2920748147e8a276e84728ab6be773f9d0e8
PradhayA <meetpradhay@gmail.com>
2023-09-23 21:41:07 +0100

    check for work

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,14 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
+from collections import defaultdict
+
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] = frequencies[item] + 1
+        else:
+            frequencies[item] = 1
     return frequencies
